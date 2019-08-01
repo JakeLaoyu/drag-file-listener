2e1af0d9c9d814e0da412a678dd851ed852ebf0a
Jake <Jake.Laoyu@gmail.com>
2019-08-02 00:57:50 +0800

    feat: upload dir

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="drag"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DragLitener from '../../libs/index.esm'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  methods: {
+    outputFile ({ files, filesTree }) {
+      console.log(files)
+      console.log(filesTree)
+    }
+  },
+  mounted () {
+    new DragLitener({
+      selector: document.querySelector('.drag'),
+      callback: this.outputFile
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.drag {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,56 +1,114 @@
+import debounce from 'lodash/debounce'
+
 class Drag {
-    constructor(options) {
-        this.dom = options.dom
-        this.callback = options.callback
-        this.$vm = options.$vm
-        this.filesPromiseFun = []
-        this.bindDrag()
+  constructor (options) {
+    if (!options.selector) {
+      throw new Error('selector is must')
     }
+    this.selector = options.selector
+    this.callback = options.callback
+    this.debounceHandleCheck = debounce(this.returnFile, 300)
+    this.debounceReadFileFinish = debounce(this.readFileFinish, 300)
+    this.files = []
+    this.filesTree = {}
 
-    bindDrag() {
-        this.dom.addEventListener('drop', this.dragHandler.bind(this))
-        this.dom.addEventListener('dragenter', this.preventDefault)
-        this.dom.addEventListener('dragover', this.preventDefault)
-        this.dom.addEventListener('dragleave', this.preventDefault)
-    }
+    this.currentDirObj = {} // path -> dirObj
+    this.bindDrag()
+  }
 
-    async dragHandler(e) {
-        e.preventDefault()
-        e.stopPropagation()
-        if (this.$vm.$isIE) {
-            this.$vm.$message.warning('请使用最新版Chrome 或 firefox 或 Edge浏览器')
-            return
-        }
-        var items = e.dataTransfer.items
-        for (var i = 0; i < items.length; i++) {
-            var item = items[i].webkitGetAsEntry()
-            if (item) {
-                this.traverseFileTree(item)
-            }
-        }
-        const files = await Promise.all(this.filesPromiseFun)
-        this.filesPromiseFun = []
-        // 回调
-        this.callback(files)
-    }
+  bindDrag () {
+    this.selector.addEventListener('drop', this.dragHandler.bind(this))
+    this.selector.addEventListener('dragenter', this.preventDefault)
+    this.selector.addEventListener('dragover', this.preventDefault)
+    this.selector.addEventListener('dragleave', this.preventDefault)
+  }
 
-    traverseFileTree(item) {
-        if (item.isFile) {
-            if (item.name.charAt(0) === '.') return
-            this.filesPromiseFun.push(new Promise(resolve => {
-                item.file(file => {
-                    resolve(file)
-                })
-            }))
-        } else if (item.isDirectory) {
-            this.$vm.$message.warning('禁止上传文件夹，请选择文件')
-        }
-    }
+  readFileFinish () {
+    Object.keys(this.currentDirObj).forEach(async path => {
+      this.currentDirObj[path].files = await Promise.all(this.currentDirObj[path].files)
+      console.log('this.currentDirObj[path].files', this.currentDirObj[path].files)
+      this.files = this.files.concat(this.currentDirObj[path].files)
+      this.debounceHandleCheck()
+    })
+  }
 
-    preventDefault(e) {
-        e.preventDefault()
-        e.stopPropagation()
+  async returnFile () {
+    // 回调
+    this.callback({
+      files: this.files,
+      filesTree: this.filesTree
+    })
+  }
+
+  dragHandler (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    if (!e.dataTransfer) {
+      console.warning('请使用最新版Chrome 或 firefox 或 Edge浏览器')
+      return
     }
+    var items = e.dataTransfer.items
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i].webkitGetAsEntry()
+      if (item) {
+        this.traverseFileTree(item)
+      }
+    }
+  }
+
+  // readEntries 每次最多返回100个，需要重复调用
+  // https://developer.mozilla.org/en-US/docs/Web/API/FileSystemDirectoryReader/readEntries
+  readerEntries (dirReader) {
+    dirReader.readEntries(entries => {
+      for (var i = 0; i < entries.length; i++) {
+        this.traverseFileTree(entries[i])
+      }
+      if (entries.length) this.readerEntries(dirReader)
+    })
+  }
+
+  traverseFileTree (item) {
+    if (item.isFile) {
+      if (item.name.charAt(0) === '.') return
+      const filePath = item.fullPath.split('/')
+      filePath.pop()
+      this.currentDirObj[filePath.join('/')].files.push(new Promise(resolve => {
+        item.file(file => {
+          resolve(file)
+        })
+      }))
+    } else if (item.isDirectory) {
+      this.createDirObj(item)
+
+      var dirReader = item.createReader()
+      this.readerEntries(dirReader)
+    }
+    this.debounceReadFileFinish()
+  }
+
+  createDirObj (dir) {
+    const { fullPath, name } = dir
+    const pathArr = fullPath.split('/')
+    if (this.currentDirObj[fullPath]) return
+
+    const dirObj = {
+      name,
+      fullPath,
+      children: [],
+      files: []
+    }
+    if (pathArr.length === 2) this.filesTree.push(dirObj)
+    console.log('this.currentDirObj', this.currentDirObj)
+    this.currentDirObj[fullPath] = dirObj
+    pathArr.pop()
+    this.currentDirObj[pathArr.join('/')] && this.currentDirObj[pathArr.join('/')].children.push(dirObj)
+    console.log('this.currentDirObj', this.currentDirObj)
+  }
+
+  preventDefault (e) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
 }
 
 export default Drag
